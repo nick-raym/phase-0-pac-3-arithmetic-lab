aaa666e4ec612e1848c5cd73efa85aa27d25f020
NickRayman <nlhrayman@gmail.com>
2023-12-17 19:48:17 -0500

    arithmetic complete

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(value1, value2){
+    return (value1+value2);
+}
+
+function subtract(value1, value2){
+    return (value1-value2);
+}
+
+function multiply(value1, value2){
+    return (value1*value2);
+}
+
+function divide(value1, value2){
+    return (value1/value2);
+}
+
+function increment(n){
+    return (++n);
+}
+
+function decrement(n){
+    return (--n);
+}
+
+function makeInt(n){
+    return parseInt(n, 10);;
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);;
+}
